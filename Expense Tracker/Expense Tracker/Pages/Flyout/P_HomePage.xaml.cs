c641ca6fff340e772714790398bc9d817e3537e1
nishchhalbakshi <nishchhalbakshi@gmail.com>
2021-12-02 03:36:17 +0530

    Added ListView as well as a basic CollectionView for homepage.

--- Expense Tracker/Expense Tracker/Pages/Flyout/P_HomePage.xaml.cs
+++ Expense Tracker/Expense Tracker/Pages/Flyout/P_HomePage.xaml.cs
@@ -14,29 +14,24 @@ namespace Expense_Tracker.Pages.Flyout
     public partial class P_HomePage : ContentPage
     {
         private ObservableCollection<Model.Expense> expenses = new ObservableCollection<Model.Expense>();
+        public ObservableCollection<Model.Expense> Expenses { get { return expenses; } }
         public P_HomePage()
         {
             InitializeComponent();
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
             UpdateExpenses();
         }
 
         private void UpdateExpenses()
         {
+            HomeExpensesCollectionView.ItemsSource = expenses;
             GetExpenses();
-            //HomeExpensesListView.ItemsSource = expenses;
-            
         }
 
         private void GetExpenses()
         {
             for (int i = 0; i < 2; i++)
             {
-                expenses.Add(new Model.Expense(i, i * 100 + 100, Model.ExpenseType.Service, "Electricity Bill"));
+                expenses.Add(new Model.Expense(i, (100 * i) + 100, Model.ExpenseType.Service, "Electricity Bill"));
             }
         }
     }
